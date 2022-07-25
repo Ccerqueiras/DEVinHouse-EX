ef023f92b838f8d2106f7684c04e7055a8ad1f60
Ccerks <ccerqueira64@.com>
2022-07-24 23:09:52 -0300

    EX 2

--- Semana 3/EX 1/index.js
+++ Semana 3/EX 1/index.js
@@ -1,6 +1,3 @@
 var listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78,17];
-let result = 0
-listaNumeros.forEach(function dobro(num){
-result = num * 2
-console.log(result)
-});
+listaNumeros.forEach( (num, i) => {listaNumeros[i] = num * 2;})
+console.log(listaNumeros)
