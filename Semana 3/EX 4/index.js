862f16b115c5bf5ca4a6ccdd0d328f6d3c3ceb6c
Ccerks <ccerqueira64@.com>
2022-07-24 23:49:51 -0300

    EX 4

---
+++ Semana 3/EX 4/index.js
@@ -0,0 +1,34 @@
+const convidado1 = {
+nome:"Caio",
+sobrenome:"Souza",
+setor:"Camarote",
+idade:26,
+};
+
+const convidado2 = {
+nome:"Victor",
+sobrenome:"Cerqueira",
+setor:"pista",
+idade:23,
+};
+
+const convidado3 = {
+nome:"Nina",
+sobrenome:"Dogs",
+setor:"camarote",
+idade:"15",
+};
+
+const convidado4 = {
+nome:"bruna",
+sobrenome:"fini",
+setor:"pista",
+idade:17,
+};
+
+const convidado5 = {
+nome:"Brenda",
+sobrenome:"Inda",
+setor:"pista",
+idade:66,
+};
