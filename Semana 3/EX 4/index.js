4b737e26298ca0147fcb8c3359dc88cc5121b8f1
Ccerks <ccerqueira64@.com>
2022-07-24 23:54:47 -0300

    EX 6

--- Semana 3/EX 4/index.js
+++ Semana 3/EX 4/index.js
@@ -1,34 +1,42 @@
 const convidado1 = {
-nome:"Caio",
-sobrenome:"Souza",
-setor:"Camarote",
-idade:26,
+    nome: "Caio",
+    sobrenome: "Souza",
+    setor: "Camarote",
+    idade: 26,
 };
 
 const convidado2 = {
-nome:"Victor",
-sobrenome:"Cerqueira",
-setor:"pista",
-idade:23,
+    nome: "Victor",
+    sobrenome: "Cerqueira",
+    setor: "pista",
+    idade: 23,
 };
 
 const convidado3 = {
-nome:"Nina",
-sobrenome:"Dogs",
-setor:"camarote",
-idade:"15",
+    nome: "Nina",
+    sobrenome: "Dogs",
+    setor: "camarote",
+    idade: "15",
 };
 
 const convidado4 = {
-nome:"bruna",
-sobrenome:"fini",
-setor:"pista",
-idade:17,
+    nome: "bruna",
+    sobrenome: "fini",
+    setor: "pista",
+    idade: 17,
 };
 
 const convidado5 = {
-nome:"Brenda",
-sobrenome:"Inda",
-setor:"pista",
-idade:66,
+    nome: "Brenda",
+    sobrenome: "Inda",
+    setor: "pista",
+    idade: 66,
 };
+
+var listaDeConvidados = [
+    convidado1,
+    convidado2,
+    convidado3,
+    convidado4,
+    convidado5
+];
