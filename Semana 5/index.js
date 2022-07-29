2b21c28b43ccc3b04f1a156ae2b5634f506f5149
Ccerqueiras <ccerqueira64@gmail.com>
2022-07-29 20:11:51 -0300

    EX 7

--- Semana 5/index.js
+++ Semana 5/index.js
@@ -1,5 +1,17 @@
-import { saudacao } from "./saudacao.js";
-import saudacaoEspecial from "./saudacao-especial.js"
+import { Pedido } from "./src/models/pedido.js"; 
+import { Produto } from "./src/models/Produto.js";
 
-console.log(saudacaoEspecial("Caio"));
+const pro1 = new Produto("Mobi", 35_000, true, 1);
+const pro2 = new Produto("corsa", 25_000, true, 12);
+const pro3 = new Produto("etios", 45_000, false, 0);
+const pro4 = new Produto("fiesta", 37_000, true, 6);
+const pro5 = new Produto("combi", 13_000, false, 0);
 
+const ped1 = new Pedido("7494091", "Caio");
+const ped2 = new Pedido("7056591", "Victor");
+ped1.adicionarProduto(pro2);
+ped1.adicionarProduto(pro1);
+ped1.adicionarProduto(pro4);
+
+ped2.adicionarProduto(pro1);
+ped2.adicionarProduto(pro4);
